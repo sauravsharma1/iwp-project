af8a55e2555b1aad14da32901af259db86e89b6d
sauravsharma1 <56475622+sauravsharma1@users.noreply.github.com>
2021-11-24 23:44:07 +0530

    Add files via upload

---
+++ author/includes/footer.inc.php
@@ -0,0 +1,10 @@
+<footer id="footer">
+  <p>All Rights Reserved by &copy; NewsBook 2021</p>
+    <h4>Made By-Saurav, Chirag, Aditya</h4>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
